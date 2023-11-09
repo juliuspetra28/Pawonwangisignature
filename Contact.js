9ee9f9ef361e3eb3e3439a56c87e825097a4ed09
juliuspetra6@gmail.com <juliuspetra6@gmail.com>
2023-11-09 22:18:57 +0700

    first commit

---
+++ Contact.js
@@ -0,0 +1,11 @@
+function Hello() {
+    alert("Terima Kasih Atas Kritikan dan Pesan Anda")}
+function Hello() {
+var fName = document.forms["cpForm"]["fname"].value;
+var lName = document.forms["cpForm"]["lname"].value;
+var subject = document.forms["cpForm"]["subject"].value;
+
+if (fName && lName && subject) {
+  alert("Terima kasih karena telah mengisi");
+}
+}
